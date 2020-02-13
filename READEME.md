4c1cac64a772b3f3da33f7ec462ef41d712b255f
sekoudosso82 <sekou.dosso82@gmail.com>
2020-02-13 13:34:52 -0500

    second commit

--- READEME.md
+++ READEME.md
@@ -3,12 +3,18 @@
 ## Objective
 The following are the user stories that must be fulfilled for a minimal viable product (MVP). 
     As a user on the system, I can:
-1) create a user account with a unique email and/or username (must have multiple users)
+
+1) create a user account with a unique username (must have multiple users)
 2) secure my account using a password
 3) log in/log out
 4) create a bank account associated with each user 
-5) deposit money
-6) withdraw money (no negative balances!)
-7) view account balance
+5) register a new account owner 
+6) open a new account
+7) deposit money (no negative amount!)
+8) withdraw money (no negative amount!)
+9) withdraw money (no negative balances!)
+10) transfer money to an account owner (no negative amount!)
+11) view account balance
+12) close active account
 
 
--- app/helper.rb
+++ app/helper.rb
@@ -12,12 +12,21 @@
   end 
 
   def login_
-    puts "=*     Please login to continue!"
-    print "=*     user_name :  "
-    name = gets.chomp
-    print "=*     password  :  "
-    pass = gets.chomp
-    user = AccountOwner.find_by(user_name: name, password: pass)
+    puts "=*     Login or Register"
+    puts "=*     1. Login to continue!"
+    puts "=*     2. Create an account"
+    choice = gets.chomp.to_i
+  
+      case (choice)
+          when 1
+              print "=*     user_name :  "
+              name = gets.chomp
+              print "=*     password  :  "
+              pass = gets.chomp
+              user = AccountOwner.find_by(user_name: name, password: pass)
+          when 2 
+            register_
+      end  
   end 
   
 
@@ -26,12 +35,12 @@
     puts "=*"*20
     puts "=*"*20
     puts "=*     What would you like to do?"
-    puts "=*     0. Make a transfert"
-    puts "=*     1. Make a deposit"
-    puts "=*     2. Withdraw some money"
-    puts "=*     3. Check balance"
-    puts "=*     4. To close your account"
-    puts "=*     5. Exit"
+    puts "=*     1. Make a transfert"
+    puts "=*     2. Make a deposit"
+    puts "=*     3. Withdraw some money"
+    puts "=*     4. Check balance"
+    puts "=*     5. To close your account"
+    puts "=*     6. Exit"
     puts "=*"*20
     puts "=*"*20
     puts
@@ -41,7 +50,7 @@
     puts "=*"*20
     puts "=*"*20
     puts "=*     Puppy bank checking balance    "
-    puts "=*     your current balance is $#{account_.balance}    " #
+    puts "=*     Your current balance is $#{account_.balance}    " 
     puts "=*     #{Time.now}      "
     puts "=*     Good bye                       "
     puts "=*"*20
@@ -49,60 +58,74 @@
   end 
 
 def transfert_(sender)
-    puts "=*     please enter the the amount to transfert"
-    trans_am = gets.chomp.to_i
-
-    puts "=*     please enter the receiver id number"
-    rec = gets.chomp.to_i
-    rec_acc = Account.find(rec)
-    if rec_acc.status != "open"
-      puts "=*     Sorry, this account is closed"
-      # get_start(owner)
-    end 
-
-    trans = Transfert.create(sender_id: sender.id, 
-                          receiver_id: rec, 
-                          trans_amount: trans_am,
-                          date: Time.now)
-    (sender.balance -= trans_am)
-    sender.save
-    (rec_acc.balance += trans_am)
-    rec_acc.save
-    # get_start(owner)
+    puts "=*     Transfert some money"
+    if sender.status != "open"
+            puts "=*     Sorry, this account is closed"
+    else             
+            puts "=*     Please enter the the amount to transfert"
+            trans_am = gets.chomp.to_i
+            if trans_am <= 0 
+              puts "=*     You can't transfer less than Zero "
+            elsif trans_am> sender.balance
+              puts "=*     Transfert should be less than your balance"
+            else
+              puts "=*     Please enter the receiver id number"
+              rec = gets.chomp.to_i
+              rec_acc = Account.find(rec)
+              if rec_acc.status != "open"
+                puts "=*     Sorry, this account is closed"
+              else
+                trans = Transfert.create(sender_id: sender.id, 
+                                      receiver_id: rec, 
+                                      trans_amount: trans_am,
+                                      date: Time.now)
+                (sender.balance -= trans_am)
+                sender.save
+                (rec_acc.balance += trans_am)
+                rec_acc.save
+              end
+            end      
+    end
 end 
 
 def deposit_(owner)
+  puts "=*     Deposit some money"
   if owner.status != "open"
-    puts "=*     Sorry, this account is closed"
-    # get_start(owner)
-  end 
-  puts "=*     please enter the amount : "
-  dep_amount = gets.chomp.to_i
-  transac = Transaction.create(account_id:owner.id, 
-                        title: "deposit", 
-                        transaction_amount: dep_amount,
-                        transaction_date: Time.now).execute
+      puts "=*     Sorry, this account is closed"
+  else 
+      puts "=*     Please enter the amount : "
+      dep_amount = gets.chomp.to_i
+      if dep_amount < 0
+        puts "=*     Deposit amount can not be less than ZERO"
+      else
+      transac = Transaction.create(account_id:owner.id, 
+                            title: "deposit", 
+                            transaction_amount: dep_amount,
+                            transaction_date: Time.now).execute
+      end
+  end
 end 
 
 def withdraw_(owner)
   puts "=*     Withdraw some money"
-                  if owner.status != "open"
-                      puts "=*     Sorry, this account is closed"
-                      # get_start(owner)
-                  end 
-                  puts "=*     Please enter the amount to withdraw"
-                  withdraw_amount = gets.chomp.to_i
-                  if withdraw_amount <= owner.balance
-                      transac = Transaction.create(account_id:owner.id, 
-                                title: "withdraw", transaction_amount: withdraw_amount,
-                                transaction_date: Time.now).execute
-                      # get_start(owner)
-                    else
-                      puts "=*"*20
-                      puts "=*     your balance is less then the withdraw amount"
-                      puts "=*"*20
-                      # get_start(owner)
-                  end
+  if owner.status != "open"
+      puts "=*     Sorry, this account is closed"    
+  else                
+      puts "=*     Please enter the amount to withdraw"
+      withdraw_amount = gets.chomp.to_i
+      if withdraw_amount < 0
+        puts "=*     Withdraw amount can not be less than ZERO"
+      
+      elsif (withdraw_amount <= owner.balance)
+         transac = Transaction.create(account_id:owner.id, 
+                   title: "withdraw", transaction_amount: withdraw_amount,
+                   transaction_date: Time.now).execute
+      else
+         puts "=*"*20
+         puts "=*     Your balance is less then the withdraw amount"
+         puts "=*"*20
+      end
+  end
 end 
 
 
@@ -113,31 +136,69 @@ def get_start(owner)
 
     menu_
    
-    choice = gets.chomp.to_i
-  
+    choice = gets.chomp.to_i 
       case (choice)
-          when 0
+          when 1
             transfert_(user_acc_info)
             get_start(owner)
 
-          when 1
+          when 2
             deposit_(user_acc_info)
             get_start(owner)
-          when 2
+          when 3
             withdraw_(user_acc_info)
             get_start(owner)
-          when 3 
+          when 4
             receipt_(user_acc_info)
             get_start(owner)     
-          when 4
-            user_acc_info.update_column(:status, "closed")
-            get_start(owner)
-          when 5 
+          when 5
+            puts "=*     WARNING you will close this account."
+            puts "=*     1. To close this account"
+            puts "=*     2. To exit"
+            choice = gets.chomp.to_i
+          
+              case (choice)
+                  when 1
+                    user_acc_info.update_column(:status, "closed")
+                  when 2 
+                    get_start(owner)
+              end  
+          when 6 
             return
+          else 
+            get_start(owner)
       end
     else
       puts "=*"*20
       puts "=*     user_name or password incorrect"
   end
 
+end 
+
+def register_
+  print "=*     Please enter your user_name      :  "
+  new_user_name = gets.chomp
+  print "=*     Please enter your password       :  "
+  new_user_password = gets.chomp
+  print "=*     Please enter your phone num      :  "
+  new_user_phone = gets.chomp.to_i
+  print "=*     Please enter your initial amount :  "
+  new_user_balance = gets.chomp.to_i
+  nu = AccountOwner.create(user_name: new_user_name, 
+                      password: new_user_password, 
+                      phone: new_user_phone)
+
+  Account.create(account_owner_id: nu.id, 
+                 balance: new_user_balance, 
+                 status: "open")
+end 
+
+def open_account_(owner)
+  
+  print "=*     Please enter your initial amount :  "
+  new_user_balance = gets.chomp.to_i
+  
+  Account.create(account_owner_id: owner.id, 
+                 balance: new_user_balance, 
+                 status: "open")
 end 
